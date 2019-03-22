038607ec9153163cec36ba1adde8e9919e6e9a0d
seungwon0106 <48343593+seungwon0106@users.noreply.github.com>
2019-03-22 16:02:43 +0900

    msgQiz
    메세지큐 활용

---
+++ msgQizRx.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+// For IPC function
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+
+struct myData
+{
+	long int msgType; //default 
+	char name[20];
+	int num;
+	int kor;
+	int eng;
+	int mat;
+};
+
+
+int main(void)
+{
+	int running = 1;
+	struct myData person[10];
+	struct msqid_ds msqstat;  //남은 메시지큐의 메시지 갯수를 확인하기 위해
+	int msgid;
+	int personNum=0;
+	int total =0;
+	float ave =0;
+
+	//step0. ftok();
+
+	//step1. msgget 
+	msgid = msgget((key_t)1234, 0666 | IPC_CREAT);
+	if (msgid == -1)
+	{
+		fprintf(stderr, "Error:msgget failed : %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	while (running)
+	{
+		printf("personNum++11 : %d\n", personNum);
+		//step2. msgrcv
+		if (msgrcv(msgid, &person[personNum], sizeof(person)-sizeof(long), 0, 0) == -1)
+		{
+			fprintf(stderr, "Error:msgrcv failed : %d\n", errno);
+			exit(EXIT_FAILURE);
+		}
+		printf("personNum++22 : %d\n", personNum);
+
+		if (person[personNum].msgType == 2)
+		{
+			running = 0;
+		}
+		else
+		{
+			printf("personNum++33 : %d\n", personNum);
+			//printf("Receive Data Person[%d]\n", personNum);
+			total = person[personNum].kor + person[personNum].eng + person[personNum].mat;
+			ave = (total / 3);
+			printf("personnum : %d\n", personNum);
+			printf("Name :%s, total : %d, ave : %f\n", person[personNum].name, total, ave);
+			
+		}
+
+		if (msgctl(msgid, IPC_STAT, &msqstat) == -1)
+		{
+			perror("Fail:msgctl()");
+			exit(1);
+		}
+		//printf("remain message count:%d\n", msqstat.msg_qnum);
+		if (msqstat.msg_qnum > 0)
+			running = 1;
+
+		personNum++;
+		printf("personNum++ : %d\n", personNum);
+	}
+
+	//step3. msgctl IPC_RMID
+	if (msgctl(msgid, IPC_RMID, 0) == -1)
+	{
+		fprintf(stderr, "Error:msgctl IPC_RMID failed : %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
---
+++ msgQizTx.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+//For IPC Function
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+
+typedef struct 
+{
+	long int msgType; //default 
+	char name[20];
+	int num;
+	int kor;
+	int eng;
+	int mat;
+}MYDATA;
+
+
+int main(void)
+{
+	int running = 1;
+	MYDATA person[10];
+	int msgid;
+	int personNum = 0;
+	//char buffer[BUFSIZ];
+
+	//step0. ftok();
+
+	//step1. msgget 
+	msgid = msgget((key_t)1234, 0666 | IPC_CREAT);
+	if (msgid == -1)
+	{
+		fprintf(stderr, "Error:msgget failed : %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	while (running)
+	{
+		printf("Name:");
+		fgets(person[personNum].name, sizeof(person[personNum].name), stdin);	//fgets(저장될 주소값, 사이즈, 파일 스트림);
+
+		if (!strncmp(person[personNum].name, "end", 3))
+		{
+			person[personNum].msgType = 2;
+			running = 0;
+		}
+		else
+		{
+			printf("Number :");
+			scanf("%d", &person[personNum].num);
+			while (getchar() != '\n');
+			printf("Korean :");
+			scanf("%d", &person[personNum].kor);
+			while (getchar() != '\n');
+			printf("English :");
+			scanf("%d", &person[personNum].eng);
+			while (getchar() != '\n');
+			printf("Math :");
+			scanf("%d", &person[personNum].mat);
+			person[personNum].msgType = 1;
+
+			//입력 버퍼 비우기
+			while (getchar() != '\n');
+		}
+
+
+		//step2. msgsnd
+		if (msgsnd(msgid, &person[personNum], sizeof(MYDATA) - sizeof(long), 0) == -1)
+		{
+			//메시지가 정상적으로 전달되지 않은 경우
+			fprintf(stderr, "Error:msgsnd failed : %d\n", errno);
+			exit(EXIT_FAILURE);
+		}
+		personNum++;
+
+	}
+	exit(EXIT_SUCCESS);
+}
